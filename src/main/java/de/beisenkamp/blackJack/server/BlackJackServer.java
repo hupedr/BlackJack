ad9a4e153957e42cd639a0daabd5d1908b5b96c7
simonwesthues <westhuessimon@gmail.com>
2025-03-18 12:02:33 +0100

    Simon

--- src/main/java/de/beisenkamp/blackJack/server/BlackJackServer.java
+++ src/main/java/de/beisenkamp/blackJack/server/BlackJackServer.java
@@ -3,5 +3,6 @@ package de.beisenkamp.blackJack.server;
 public class BlackJackServer {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Simon ist hier!!!");
     }
 }
