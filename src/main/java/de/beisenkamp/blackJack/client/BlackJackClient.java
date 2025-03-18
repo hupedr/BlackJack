b97d845cbad2ea265c2ca51f9455ad3eeec4115d
hupedr <hupe@beisenkamp.eu>
2025-03-18 11:46:59 +0100

    Test

--- src/main/java/de/beisenkamp/blackJack/client/BlackJackClient.java
+++ src/main/java/de/beisenkamp/blackJack/client/BlackJackClient.java
@@ -2,6 +2,7 @@ package de.beisenkamp.blackJack.client;
 
 public class BlackJackClient {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hello world!!!");
     }
 }
