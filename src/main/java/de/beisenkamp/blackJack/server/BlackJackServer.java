bd0d323ed76922090c60fe65a71f0fbbb8ff832a
Philiboiii <philipp.g.treide@gmail.com>
2025-03-18 12:11:42 +0100

    Guck in meinen Branch

--- src/main/java/de/beisenkamp/blackJack/server/BlackJackServer.java
+++ src/main/java/de/beisenkamp/blackJack/server/BlackJackServer.java
@@ -4,4 +4,7 @@ public class BlackJackServer {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public static void main(String[] args) {
+        System.out.println("Hello Philipp!");
+    }
 }
