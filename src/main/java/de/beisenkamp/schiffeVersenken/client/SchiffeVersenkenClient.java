5b0d21cc5f5d2650b2e18f8f39483e370afc5d46
kianavgl <kikiv6307@gmail.com>
2025-03-18 13:05:25 +0100

    Kianaaaa

--- src/main/java/de/beisenkamp/schiffeVersenken/client/SchiffeVersenkenClient.java
+++ src/main/java/de/beisenkamp/schiffeVersenken/client/SchiffeVersenkenClient.java
@@ -1,10 +1,4 @@
 package de.beisenkamp.schiffeVersenken.client;
 
 public class SchiffeVersenkenClient {
-    public static void main(String[] args) {
-
-        System.out.println("Hello world!!!");
-        System.out.println("Hello Nelly! :)");
-        System.out.println("Hello Nelly!");
-    }
 }
---
+++ src/main/java/de/beisenkamp/schiffeVersenken/client/SchiffeVersenkenController.java
@@ -0,0 +1,8 @@
+package de.beisenkamp.schiffeVersenken.client;
+
+public class SchiffeVersenkenController {
+    public static void main(String[] args)
+    {
+
+    }
+}
