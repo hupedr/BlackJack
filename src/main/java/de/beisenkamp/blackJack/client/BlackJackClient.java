f3a8e4810244d58314c0dd08123976309748185e
hupedr <hupe@beisenkamp.eu>
2025-03-18 11:30:59 +0100

    project init

---
+++ src/main/java/de/beisenkamp/blackJack/client/BlackJackClient.java
@@ -0,0 +1,7 @@
+package de.beisenkamp.blackJack.client;
+
+public class BlackJackClient {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/de/beisenkamp/blackJack/server/BlackJackServer.java
@@ -0,0 +1,7 @@
+package de.beisenkamp.blackJack.server;
+
+public class BlackJackServer {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
